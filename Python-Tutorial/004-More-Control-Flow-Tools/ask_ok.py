35b5e4db9970f192dd9d4132e3e236680a0bdb0d
David Stevenson <drstevenson1958@yahoo.com>
2024-10-05 15:59:24 -0400

    4.9.1. Default Argument Values

---
+++ Python-Tutorial/004-More-Control-Flow-Tools/ask_ok.py
@@ -0,0 +1,16 @@
+def ask_ok(prompt, retries=4, reminder="Please try again!"):
+    while True:
+        reply = input(prompt)
+        if reply in {"y", "ye", "yes"}:
+            return True
+        if reply in {"n", "no", "nop", "nope"}:
+            return False
+        retries = retries - 1
+        if retries < 0:
+            raise ValueError("invalid user response")
+        print(reminder)
+
+
+ask_ok("Enter yes or no: ")
+ask_ok("Enter affirmative (yes) or negative (no): ", 2)
+ask_ok("Enter yes or no: ", 3, "Try again!")
