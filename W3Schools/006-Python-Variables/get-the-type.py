d76cab19313844163e6dd02d1849f7b029fdf046
David Stevenson <drstevenson1958@yahoo.com>
2025-01-11 16:05:19 -0500

    W3Schools: 006-Python-Variables: Get the Type
    You can get the data type of a variable with the type() function.

---
+++ W3Schools/006-Python-Variables/get-the-type.py
@@ -0,0 +1,12 @@
+# Get the Type
+# You can get the data type of a variable with the type() function.
+
+# Example
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+# $ python3 get-the-type.py 
+# <class 'int'>
+# <class 'str'>
