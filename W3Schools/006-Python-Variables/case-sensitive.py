519babecc5e64970f3d3158778c950a4be07a74c
David Stevenson <drstevenson1958@yahoo.com>
2025-01-11 16:07:48 -0500

    W3Schools: 006-Python-Variables: Variable names are case sensitive

---
+++ W3Schools/006-Python-Variables/case-sensitive.py
@@ -0,0 +1,13 @@
+# Case-Sensitive
+# Variable names are case-sensitive.
+
+# Example
+# This will create two variables:
+
+a = 4
+A = "Sally"
+#A will not overwrite a
+print("a", a, "A", A)
+
+# $ python3 case-sensitive.py 
+# a 4 A Sally
