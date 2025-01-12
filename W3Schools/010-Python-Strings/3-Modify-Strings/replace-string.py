f6447cbdf20a776ad0fc806e63bbf6213c666511
David Stevenson <drstevenson1958@yahoo.com>
2025-01-12 11:36:53 -0500

    W3Schools: 010-Python-Strings: Replace String

---
+++ W3Schools/010-Python-Strings/3-Modify-Strings/replace-string.py
@@ -0,0 +1,9 @@
+# Replace String
+# Example
+# The replace() method replaces a string with another string:
+
+a = "Hello, World!"
+print(a.replace("H", "J"))
+
+# $ python3 replace-string.py 
+# Jello, World!
