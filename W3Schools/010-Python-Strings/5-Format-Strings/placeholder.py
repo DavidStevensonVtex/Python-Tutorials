0da6a2a5074811a9268fadcbd602d7aa055302da
David Stevenson <drstevenson1958@yahoo.com>
2025-01-12 11:51:22 -0500

    W3Schools: 010-Python-Strings: Format Strings: Placeholders for variables

---
+++ W3Schools/010-Python-Strings/5-Format-Strings/placeholder.py
@@ -0,0 +1,12 @@
+# Placeholders and Modifiers
+# A placeholder can contain variables, operations, functions, and modifiers to format the value.
+
+# Example
+# Add a placeholder for the price variable:
+
+price = 59
+txt = f"The price is {price} dollars"
+print(txt)
+
+# $ python3 placeholder.py 
+# The price is 59 dollars
