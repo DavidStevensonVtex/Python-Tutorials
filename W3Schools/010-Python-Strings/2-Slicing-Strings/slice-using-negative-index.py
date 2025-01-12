456ce6c3361cf6ecf2027f109f5442ffcb8bb726
David Stevenson <drstevenson1958@yahoo.com>
2025-01-12 11:28:57 -0500

    W3Schools: 010-Python-Slices: Slice using negative indexes

---
+++ W3Schools/010-Python-Strings/2-Slicing-Strings/slice-using-negative-index.py
@@ -0,0 +1,15 @@
+# Negative Indexing
+# Use negative indexes to start the slice from the end of the string:
+
+# Example
+# Get the characters:
+
+# From: "o" in "World!" (position -5)
+
+# To, but not included: "d" in "World!" (position -2):
+
+b = "Hello, World!"
+print(b[-5:-2])
+
+# $ python3 slice-using-negative-index.py 
+# orl
