40a9205fce916178f8304a7c0ec33cfa5441ad20
David Stevenson <drstevenson1958@yahoo.com>
2025-01-12 11:38:53 -0500

    W3Schools: 010-Python-Strings: Split String

---
+++ W3Schools/010-Python-Strings/3-Modify-Strings/split-string.py
@@ -0,0 +1,8 @@
+# Split String
+# The split() method returns a list where the text between the specified separator becomes the list items.
+
+# Example
+# The split() method splits the string into substrings if it finds instances of the separator:
+
+a = "Hello, World!"
+print(a.split(",")) # returns ['Hello', ' World!']
