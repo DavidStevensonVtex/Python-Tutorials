9fa42fda6c4be4de3156a6cd5f6cbaaa6fd6cca0
David Stevenson <drstevenson1958@yahoo.com>
2025-01-12 11:32:14 -0500

    W3Schools: 010-Python-Strings: Upper Case

---
+++ W3Schools/010-Python-Strings/3-Modify-Strings/upper-case.py
@@ -0,0 +1,9 @@
+# Upper Case
+# ExampleGet your own Python Server
+# The upper() method returns the string in upper case:
+
+a = "Hello, World!"
+print(a.upper())
+
+# $ python3 upper-case.py 
+# HELLO, WORLD!
