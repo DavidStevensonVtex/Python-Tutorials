864ddf26a2ac9066cdc12643d980a809ebf4557f
David Stevenson <drstevenson1958@yahoo.com>
2025-01-12 11:54:17 -0500

    W3Schools: 010-Python-Strings: Format Strings: Placeholder containing expression

---
+++ W3Schools/010-Python-Strings/5-Format-Strings/placeholder-containing-expression.py
@@ -0,0 +1,10 @@
+# A placeholder can contain Python code, like math operations:
+
+# Example
+# Perform a math operation in the placeholder, and return the result:
+
+txt = f"The price is {20 * 59} dollars"
+print(txt)
+
+# $ python3 placeholder-containing-expression.py 
+# The price is 1180 dollars
