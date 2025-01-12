894f082dbfe22deed5e9662c1b2efbddd9dd4ab2
David Stevenson <drstevenson1958@yahoo.com>
2025-01-12 11:15:19 -0500

    W3Schools: 010-Python-Strings: Looping through a string

---
+++ W3Schools/010-Python-Strings/1-Python-Strings/looping-through-a-string.py
@@ -0,0 +1,16 @@
+# Looping Through a String
+# Since strings are arrays, we can loop through the characters in a string, with a for loop.
+
+# Example
+# Loop through the letters in the word "banana":
+
+for x in "banana":
+  print(x)
+
+# $ python3 looping-through-a-string.py 
+# b
+# a
+# n
+# a
+# n
+# a
