4fd9e4d8c8beed7b62f73bffc82913f5e36ccade
David Stevenson <drstevenson1958@yahoo.com>
2025-01-12 11:43:21 -0500

    W3Schools: 10-Python-Strings: String Concatenation
    To concatenate, or combine, two strings you can use the + operator.

---
+++ W3Schools/010-Python-Strings/4-Concatenate-Strings/string-concatenation.py
@@ -0,0 +1,13 @@
+# String Concatenation
+# To concatenate, or combine, two strings you can use the + operator.
+
+# ExampleGet your own Python Server
+# Merge variable a with variable b into variable c:
+
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+
+# $ python3 string-concatenation.py 
+# HelloWorld
