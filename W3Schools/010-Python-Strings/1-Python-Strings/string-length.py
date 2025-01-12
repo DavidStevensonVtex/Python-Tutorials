9ee2b520f86d75e2ab10deb72fd6b2c759832888
David Stevenson <drstevenson1958@yahoo.com>
2025-01-12 11:16:42 -0500

    W3Schools: 010-Python-Strings: String Length

---
+++ W3Schools/010-Python-Strings/1-Python-Strings/string-length.py
@@ -0,0 +1,11 @@
+# String Length
+# To get the length of a string, use the len() function.
+
+# Example
+# The len() function returns the length of a string:
+
+a = "Hello, World!"
+print(len(a))
+
+# $ python3 string-length.py 
+# 13
