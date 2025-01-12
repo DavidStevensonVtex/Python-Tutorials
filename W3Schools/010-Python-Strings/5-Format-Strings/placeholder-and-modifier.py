7a0745e3e79789e6d83645d0de61d7a6853c5df7
David Stevenson <drstevenson1958@yahoo.com>
2025-01-12 11:52:50 -0500

    W3Schools: 010-Python-Strings: Format Strings: Placeholder and Modifier example

---
+++ W3Schools/010-Python-Strings/5-Format-Strings/placeholder-and-modifier.py
@@ -0,0 +1,9 @@
+# Example
+# Display the price with 2 decimals:
+
+price = 59
+txt = f"The price is {price:.2f} dollars"
+print(txt)
+
+# $ python3 placeholder-and-modifier.py 
+# The price is 59.00 dollars
