7370d1bdc14b42599a62fad6154f41a10c045b31
David Stevenson <drstevenson1958@yahoo.com>
2025-01-12 11:49:13 -0500

    W3Schools: 010-Python-Strings: Format Strings using f-string

---
+++ W3Schools/010-Python-Strings/5-Format-Strings/f-strings.py
@@ -0,0 +1,14 @@
+# F-Strings
+# F-String was introduced in Python 3.6, and is now the preferred way of formatting strings.
+
+# To specify a string as an f-string, simply put an f in front of the string literal, and add curly brackets {} as placeholders for variables and other operations.
+
+# Example
+# Create an f-string:
+
+age = 36
+txt = f"My name is John, I am {age}"
+print(txt)
+
+# $ python3 f-strings.py
+# My name is John, I am 36
