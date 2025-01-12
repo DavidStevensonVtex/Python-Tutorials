559e56f8face0c473638c9f07474cd939ccd877f
David Stevenson <drstevenson1958@yahoo.com>
2025-01-12 11:26:29 -0500

    W3Schools: 010-Python-Strings: Slice to the end

---
+++ W3Schools/010-Python-Strings/2-Slicing-Strings/slice-to-the-end.py
@@ -0,0 +1,11 @@
+# Slice To the End
+# By leaving out the end index, the range will go to the end:
+
+# Example
+# Get the characters from position 2, and all the way to the end:
+
+b = "Hello, World!"
+print(b[2:])
+
+# $ python3 slice-to-the-end.py 
+# llo, World!
