69abac363d46a5e095cd192adac0aa130e2e737f
David Stevenson <drstevenson1958@yahoo.com>
2025-01-13 15:35:42 -0500

    W3Schools: 013-Python-Lists: Access List Items

---
+++ W3Schools/013-Python-Lists/02-Access-List-Items/access-list-items.py
@@ -0,0 +1,8 @@
+# Access Items
+# List items are indexed and you can access them by referring to the index number:
+
+# ExampleGet your own Python Server
+# Print the second item of the list:
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist[1])
---
+++ W3Schools/013-Python-Lists/02-Access-List-Items/range-of-indexes.py
@@ -0,0 +1,10 @@
+# Range of Indexes
+# You can specify a range of indexes by specifying where to start and where to end the range.
+
+# When specifying a range, the return value will be a new list with the specified items.
+
+# Example
+# Return the third, fourth, and fifth item:
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5])
