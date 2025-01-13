02dce5540f5fe75d9ed16cad462845d774a420ed
David Stevenson <drstevenson1958@yahoo.com>
2025-01-13 14:41:00 -0500

    W3Schools: 011-Python-Booleans: Functions can return boolean values

---
+++ W3Schools/011-Python-Booleans/functions-can-return-boolean.py
@@ -0,0 +1,13 @@
+# Functions can Return a Boolean
+# You can create functions that returns a Boolean Value:
+
+# Example
+# Print the answer of a function:
+
+def myFunction() :
+  return True
+
+print(myFunction())
+
+# $ python3 functions-can-return-boolean.py 
+# True
