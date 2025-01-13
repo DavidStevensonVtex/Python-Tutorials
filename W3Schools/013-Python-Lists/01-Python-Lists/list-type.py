9a563ec6a100f82ac33c131d56225423cbcdae30
David Stevenson <drstevenson1958@yahoo.com>
2025-01-13 15:22:59 -0500

    W3Schools: 013-Python-Lists: lists are defined as objects with the data type 'list'

---
+++ W3Schools/013-Python-Lists/01-Python-Lists/list-type.py
@@ -0,0 +1,12 @@
+# type()
+# From Python's perspective, lists are defined as objects with the data type 'list':
+
+# <class 'list'>
+# Example
+# What is the data type of a list?
+
+mylist = ["apple", "banana", "cherry"]
+print(type(mylist))
+
+# $ python3 list-type.py
+# <class 'list'>
