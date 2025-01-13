629796f7fb25da99ff9febd7be3b23e5f9c958fb
David Stevenson <drstevenson1958@yahoo.com>
2025-01-13 14:34:01 -0500

    W3Schools: 011-Python-Booleans: convert values and variables to bool

---
+++ W3Schools/011-Python-Booleans/evaluate-values-and-variables.py
@@ -0,0 +1,25 @@
+# Evaluate Values and Variables
+
+# The bool() function allows you to evaluate any value, and give you True or False in return,
+
+# Example
+# Evaluate a string and a number:
+
+print(bool("Hello"))
+print(bool(15))
+
+# Example
+
+# Evaluate two variables:
+
+x = "Hello"
+y = 15
+
+print(bool(x))
+print(bool(y))
+
+# $ python3 evaluate-values-and-variables.py 
+# True
+# True
+# True
+# True
