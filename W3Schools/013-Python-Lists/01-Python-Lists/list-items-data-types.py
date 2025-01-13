4719729b0f81bb5c9be5aa3382776456300e96d2
David Stevenson <drstevenson1958@yahoo.com>
2025-01-13 15:19:34 -0500

    W3Schools: 013-Python-Lists: List items can be of any data type

---
+++ W3Schools/013-Python-Lists/01-Python-Lists/list-items-data-types.py
@@ -0,0 +1,9 @@
+# List Items - Data Types
+# List items can be of any data type:
+
+# Example
+# String, int and boolean data types:
+
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
