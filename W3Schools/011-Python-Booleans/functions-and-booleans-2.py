0629fc233a65e0c60c5895d7aa62e704fa219e4a
David Stevenson <drstevenson1958@yahoo.com>
2025-01-13 14:49:03 -0500

    W3Schools: 011-Python-Booleans- Functions and booleans example 2

---
+++ W3Schools/011-Python-Booleans/functions-and-booleans-2.py
@@ -0,0 +1,13 @@
+# Example
+# Print "YES!" if the function returns True, otherwise print "NO!":
+
+def myFunction() :
+  return True
+
+if myFunction():
+  print("YES!")
+else:
+  print("NO!")
+
+# $ python3 functions-and-booleans-2.py 
+# YES!
