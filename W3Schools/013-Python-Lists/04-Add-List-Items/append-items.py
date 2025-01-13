d1c5920ca36cec7e739da9a654f75229b30cd8f3
David Stevenson <drstevenson1958@yahoo.com>
2025-01-13 15:55:52 -0500

    W3Schools: 013-Python-Lists: Add List Items

---
+++ W3Schools/013-Python-Lists/04-Add-List-Items/append-items.py
@@ -0,0 +1,12 @@
+# Append Items
+# To add an item to the end of the list, use the append() method:
+
+# ExampleGet your own Python Server
+# Using the append() method to append an item:
+
+thislist = ["apple", "banana", "cherry"]
+thislist.append("orange")
+print(thislist)
+
+# $ python3 append-items.py 
+# ['apple', 'banana', 'cherry', 'orange']
---
+++ W3Schools/013-Python-Lists/04-Add-List-Items/insert-items.py
@@ -0,0 +1,14 @@
+# Insert Items
+# To insert a list item at a specified index, use the insert() method.
+
+# The insert() method inserts an item at the specified index:
+
+# Example
+# Insert an item as the second position:
+
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(1, "orange")
+print(thislist)
+
+# $ python3 insert-items.py 
+# ['apple', 'orange', 'banana', 'cherry']
