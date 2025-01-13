f4018cb711dd36d970615090c219ae2467313e2e
David Stevenson <drstevenson1958@yahoo.com>
2025-01-13 15:18:31 -0500

    W3Schools: 013-Python-Lists: list length

---
+++ W3Schools/013-Python-Lists/01-Python-Lists/list-length.py
@@ -0,0 +1,11 @@
+# List Length
+# To determine how many items a list has, use the len() function:
+
+# Example
+# Print the number of items in the list:
+
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist))
+
+# $ python3 list-length.py 
+# 3
