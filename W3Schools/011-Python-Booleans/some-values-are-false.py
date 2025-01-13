f0aa6a113bd8ba31c8d1ea9ad0ea33ec6d03febd
David Stevenson <drstevenson1958@yahoo.com>
2025-01-13 14:37:20 -0500

    W3Schools: 011-Python-Booleans: Some Values are False

---
+++ W3Schools/011-Python-Booleans/some-values-are-false.py
@@ -0,0 +1,30 @@
+# Some Values are False
+
+# In fact, there are not many values that evaluate to False, except empty values, such as (), [], {}, "", the number 0, and the value None. And of course the value False evaluates to False.
+
+# Example
+
+# The following will return False:
+
+bool(False)
+bool(None)
+bool(0)
+bool("")
+bool(())
+bool([])
+bool({})
+
+# >>> bool(False)
+# False
+# >>> bool(None)
+# False
+# >>> bool(0)
+# False
+# >>> bool("")
+# False
+# >>> bool(())
+# False
+# >>> bool([])
+# False
+# >>> bool({})
+# False
