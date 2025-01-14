f0c663e86616e6949dfa0bae90665d3a1a10b281
David Stevenson <drstevenson1958@yahoo.com>
2025-01-14 13:30:13 -0500

    W3Schools: 014-Python-Tuples: Tuple items can be of any data type
    A tuple can contain different data types

---
+++ W3Schools/014-Python-Tuples/1-Python-Tuples/tuple-items-data-types.py
@@ -0,0 +1,16 @@
+# Tuple Items - Data Types
+# Tuple items can be of any data type:
+
+# Example
+# String, int and boolean data types:
+
+tuple1 = ("apple", "banana", "cherry")
+tuple2 = (1, 5, 7, 9, 3)
+tuple3 = (True, False, False)
+
+# A tuple can contain different data types:
+
+# Example
+# A tuple with strings, integers and boolean values:
+
+tuple1 = ("abc", 34, True, 40, "male")
