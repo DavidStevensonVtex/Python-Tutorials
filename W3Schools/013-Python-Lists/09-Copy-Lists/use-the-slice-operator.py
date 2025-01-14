c89620cae481e69bcf39327e901fb370f8a67350
David Stevenson <drstevenson1958@yahoo.com>
2025-01-14 13:07:10 -0500

    W3Schools: 013-Python-Lists: Copy List: use the slice operator

---
+++ W3Schools/013-Python-Lists/09-Copy-Lists/use-the-slice-operator.py
@@ -0,0 +1,12 @@
+# Use the slice Operator
+# You can also make a copy of a list by using the : (slice) operator.
+
+# Example
+# Make a copy of a list with the : operator:
+
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist[:]
+print(mylist)
+
+# $ python3 use-the-slice-operator.py 
+# ['apple', 'banana', 'cherry']
