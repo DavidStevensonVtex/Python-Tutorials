36b0812bceb105e0cff76987c518e04dcd52ffb9
David Stevenson <drstevenson1958@yahoo.com>
2025-01-14 13:31:26 -0500

    W3Schools: 014-Python-Tuples: Tuple Type

---
+++ W3Schools/014-Python-Tuples/1-Python-Tuples/tuple-type.py
@@ -0,0 +1,15 @@
+# type()
+
+# From Python's perspective, tuples are defined as objects with the data type 'tuple':
+
+# <class 'tuple'>
+
+# Example
+
+# What is the data type of a tuple?
+
+mytuple = ("apple", "banana", "cherry")
+print(type(mytuple))
+
+# $ python3 tuple-type.py
+# <class 'tuple'>
