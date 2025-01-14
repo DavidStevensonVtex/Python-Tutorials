ef107f547c73e59b2d797af2250e263e1dbb6662
David Stevenson <drstevenson1958@yahoo.com>
2025-01-14 13:49:36 -0500

    W3Schools: 014-Python-Tuples_ Access Tuples using Range of Negative Indexes (slice)

---
+++ W3Schools/014-Python-Tuples/2-Access-Tuples/range-of-negative-indexes.py
@@ -0,0 +1,11 @@
+# Range of Negative Indexes
+# Specify negative indexes if you want to start the search from the end of the tuple:
+
+# Example
+# This example returns the items from index -4 (included) to index -1 (excluded)
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[-4:-1])
+
+# $ python3 range-of-negative-indexes.py 
+# ('orange', 'kiwi', 'melon')
