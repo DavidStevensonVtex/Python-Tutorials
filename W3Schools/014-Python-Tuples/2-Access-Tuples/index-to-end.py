7f70a1768dc7de3c10ad2e88f2d30d92fe386ffa
David Stevenson <drstevenson1958@yahoo.com>
2025-01-14 13:46:32 -0500

    W3Schools: 014-Python-Tuples: Access Tuples from Index to End (slice)

---
+++ W3Schools/014-Python-Tuples/2-Access-Tuples/index-to-end.py
@@ -0,0 +1,10 @@
+# By leaving out the end value, the range will go on to the end of the tuple:
+
+# Example
+# This example returns the items from "cherry" and to the end:
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:])
+
+# $ python3 index-to-end.py 
+# ('cherry', 'orange', 'kiwi', 'melon', 'mango')
