82d24b3664bd599a699a392ef86922c6f49bfd98
David Stevenson <drstevenson1958@yahoo.com>
2025-01-14 13:42:57 -0500

    W3Schools: 014-Python-Tuuples: Access Tuples using a Range of Indexes (slice)

---
+++ W3Schools/014-Python-Tuples/2-Access-Tuples/range-of-indexes.py
@@ -0,0 +1,13 @@
+# Range of Indexes
+# You can specify a range of indexes by specifying where to start and where to end the range.
+
+# When specifying a range, the return value will be a new tuple with the specified items.
+
+# Example
+# Return the third, fourth, and fifth item:
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:5])
+
+# $ python3 range-of-indexes.py 
+# ('cherry', 'orange', 'kiwi')
