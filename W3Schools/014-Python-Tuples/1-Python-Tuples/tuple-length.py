e6429b95f26382df01d3ac73f0a6d9b6f765dfe8
David Stevenson <drstevenson1958@yahoo.com>
2025-01-14 13:24:52 -0500

    W3Schools: 014-Python-Tuples: Tuple Length

---
+++ W3Schools/014-Python-Tuples/1-Python-Tuples/tuple-length.py
@@ -0,0 +1,11 @@
+# Tuple Length
+# To determine how many items a tuple has, use the len() function:
+
+# Example
+# Print the number of items in the tuple:
+
+thistuple = ("apple", "banana", "cherry")
+print(len(thistuple))
+
+# $ python3 tuple-length.py 
+# 3
