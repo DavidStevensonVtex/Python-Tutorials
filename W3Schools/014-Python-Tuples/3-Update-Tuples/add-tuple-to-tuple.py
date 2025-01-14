da917a629547899400b5162b51a17261cebbfbde
David Stevenson <drstevenson1958@yahoo.com>
2025-01-14 14:04:22 -0500

    W3Schools: 014-Python-Tuples: Update Tuples: Add Tuple to Tuple

---
+++ W3Schools/014-Python-Tuples/3-Update-Tuples/add-tuple-to-tuple.py
@@ -0,0 +1,13 @@
+# 2. Add tuple to a tuple. You are allowed to add tuples to tuples, so if you want to add one item, (or many), create a new tuple with the item(s), and add it to the existing tuple:
+
+# Example
+# Create a new tuple with the value "orange", and add that tuple:
+
+thistuple = ("apple", "banana", "cherry")
+y = ("orange",)
+thistuple += y
+
+print(thistuple)
+
+# $ python3 add-tuple-to-tuple.py 
+# ('apple', 'banana', 'cherry', 'orange')
