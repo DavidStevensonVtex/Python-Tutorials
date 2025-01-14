79a3656936482770e5d5f7050859f32ebbf8a839
David Stevenson <drstevenson1958@yahoo.com>
2025-01-14 13:51:40 -0500

    W3Schools: 014-Python-Tuples: Check if value is present in the tuple

---
+++ W3Schools/014-Python-Tuples/2-Access-Tuples/check-if-item-exists.py
@@ -0,0 +1,12 @@
+# Check if Item Exists
+# To determine if a specified item is present in a tuple use the in keyword:
+
+# Example
+# Check if "apple" is present in the tuple:
+
+thistuple = ("apple", "banana", "cherry")
+if "apple" in thistuple:
+  print("Yes, 'apple' is in the fruits tuple")
+
+# $ python3 check-if-item-exists.py 
+# Yes, 'apple' is in the fruits tuple
