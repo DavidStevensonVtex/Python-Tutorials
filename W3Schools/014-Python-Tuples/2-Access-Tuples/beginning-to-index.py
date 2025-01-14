7f9b5b9ff7ab5ebedbb643231ec4f0119f3a7a11
David Stevenson <drstevenson1958@yahoo.com>
2025-01-14 13:44:54 -0500

    W3Schools: 014-Python-Tuples: Access Tuples from Beginning to Index (slice)

---
+++ W3Schools/014-Python-Tuples/2-Access-Tuples/beginning-to-index.py
@@ -0,0 +1,8 @@
+# Example
+# This example returns the items from the beginning to, but NOT included, "kiwi":
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[:4])
+
+# $ python3 beginning-to-index.py 
+# ('apple', 'banana', 'cherry', 'orange')
