f378227958da2736ef1133a61ebfa36775218143
David Stevenson <drstevenson1958@yahoo.com>
2025-01-14 14:42:19 -0500

    W3Schools: 014-Python-Tuples: Join Tuples

---
+++ W3Schools/014-Python-Tuples/6-Join-Tuples/join-two-tuples.py
@@ -0,0 +1,14 @@
+# Join Two Tuples
+# To join two or more tuples you can use the + operator:
+
+# ExampleGet your own Python Server
+# Join two tuples:
+
+tuple1 = ("a", "b" , "c")
+tuple2 = (1, 2, 3)
+
+tuple3 = tuple1 + tuple2
+print(tuple3)
+
+# $ python3 join-two-tuples.py 
+# ('a', 'b', 'c', 1, 2, 3)
