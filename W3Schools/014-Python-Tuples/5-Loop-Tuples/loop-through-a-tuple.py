544206f74352eb17c46c07c3ccbb1fdee415a446
David Stevenson <drstevenson1958@yahoo.com>
2025-01-14 14:26:17 -0500

    W3Schools: 014-Python-Tuples: Loop Tuples

---
+++ W3Schools/014-Python-Tuples/5-Loop-Tuples/loop-through-a-tuple.py
@@ -0,0 +1,14 @@
+# Loop Through a Tuple
+# You can loop through the tuple items by using a for loop.
+
+# ExampleGet your own Python Server
+# Iterate through the items and print the values:
+
+thistuple = ("apple", "banana", "cherry")
+for x in thistuple:
+  print(x)
+
+# $ python3 loop-through-a-tuple.py 
+# apple
+# banana
+# cherry
