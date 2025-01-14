d4698d768ee721dc30944e108bca4979fde85218
David Stevenson <drstevenson1958@yahoo.com>
2025-01-14 14:16:47 -0500

    W3Schools: 014-Python-Tuples: Unpack Tuples

---
+++ W3Schools/014-Python-Tuples/4-Unpack-Tuples/unpack-tuple.py
@@ -0,0 +1,26 @@
+# Unpacking a Tuple
+
+# When we create a tuple, we normally assign values to it. This is called "packing" a tuple:
+
+# Packing a tuple:
+
+fruits = ("apple", "banana", "cherry")
+
+# But, in Python, we are also allowed to extract the values back into variables. 
+# This is called "unpacking":
+
+# Example
+# Unpacking a tuple:
+
+fruits = ("apple", "banana", "cherry")
+
+(green, yellow, red) = fruits
+
+print(green)
+print(yellow)
+print(red)
+
+# $ python3 unpack-tuple.py 
+# apple
+# banana
+# cherry
---
+++ W3Schools/014-Python-Tuples/4-Unpack-Tuples/using-asterisk.py
@@ -0,0 +1,18 @@
+# Using Asterisk*
+# If the number of variables is less than the number of values, you can add an * to the variable name and the values will be assigned to the variable as a list:
+
+# Example
+# Assign the rest of the values as a list called "red":
+
+fruits = ("apple", "banana", "cherry", "strawberry", "raspberry")
+
+(green, yellow, *red) = fruits
+
+print(green)
+print(yellow)
+print(red)
+
+# $ python3 using-asterisk.py 
+# apple
+# banana
+# ['cherry', 'strawberry', 'raspberry']
