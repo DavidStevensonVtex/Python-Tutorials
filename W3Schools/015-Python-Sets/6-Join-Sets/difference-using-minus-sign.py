a2ff0d0fee1b0c05e823acf0b9c0df2af8ea1799
David Stevenson <drstevenson1958@yahoo.com>
2025-01-15 14:02:29 -0500

    W3Schools: 015-Python-Sets: Join Sets: Difference: The difference_update() method will also keep the items from the first set that are not in the other set, but it will change the original set instead of returning a new set.

--- W3Schools/015-Python-Sets/6-Join-Sets/difference-using-minus-sign.py
+++ W3Schools/015-Python-Sets/6-Join-Sets/difference-using-minus-sign.py
@@ -9,5 +9,8 @@ set2 = {"google", "microsoft", "apple"}
 set3 = set1 - set2
 print(set3)
 
+# Note: The - operator only allows you to join sets with sets, and not 
+# with other data types like you can with the difference() method.
+
 # $ python3 difference-using-minus-sign.py 
 # {'banana', 'cherry'}
