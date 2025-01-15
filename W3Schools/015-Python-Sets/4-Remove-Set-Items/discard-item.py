13b6f7fccd5bca8ba68a9fd843fdfa65be1ba86c
David Stevenson <drstevenson1958@yahoo.com>
2025-01-15 13:11:08 -0500

    W3Schools: 015-Python-Sets: Discard Item - Remove item by using the discard() method

---
+++ W3Schools/015-Python-Sets/4-Remove-Set-Items/discard-item.py
@@ -0,0 +1,11 @@
+# Example
+# Remove "banana" by using the discard() method:
+
+thisset = {"apple", "banana", "cherry"}
+
+thisset.discard("banana")
+
+print(thisset)
+
+# $ python3 discard-item.py 
+# {'apple', 'cherry'}
