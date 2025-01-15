921b81fee58767e1e4e412e0827b8d0ee4622241
David Stevenson <drstevenson1958@yahoo.com>
2025-01-15 13:59:21 -0500

    W3Schools: 015-Python-Sets: Join Sets: Difference: You can use the - operator instead of the difference() method, and you will get the same result.

---
+++ W3Schools/015-Python-Sets/6-Join-Sets/difference-using-minus-sign.py
@@ -0,0 +1,13 @@
+# You can use the - operator instead of the difference() method, and you will get the same result.
+
+# Example
+# Use - to join two sets:
+
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+
+set3 = set1 - set2
+print(set3)
+
+# $ python3 difference-using-minus-sign.py 
+# {'banana', 'cherry'}
