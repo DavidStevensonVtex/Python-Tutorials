d564fb45b359e6208146e61a056dd2b117323b1e
David Stevenson <drstevenson1958@yahoo.com>
2025-01-15 16:38:30 -0500

    W3Schools: 016-Python-Dictionaries: Add Items

---
+++ W3Schools/016-Python-Dictionaries/4-Add-Items/add-items.py
@@ -0,0 +1,13 @@
+# Adding Items
+# Adding an item to the dictionary is done by using a new index key and assigning a value to it:
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict["color"] = "red"
+print(thisdict)
+
+# $ python3 add-items.py 
+# {'brand': 'Ford', 'model': 'Mustang', 'year': 1964, 'color': 'red'}
