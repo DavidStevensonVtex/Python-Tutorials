1e7ad2a125f24a169acabb53bcea709723b5b74c
David Stevenson <drstevenson1958@yahoo.com>
2025-01-15 13:27:25 -0500

    W3Schools: 015-Python-Sets: Loop Items - Use for <value-variable> in <set-variable>

---
+++ W3Schools/015-Python-Sets/5-Loop-Sets/loop-items.py
@@ -0,0 +1,15 @@
+# Loop Items
+# You can loop through the set items by using a for loop:
+
+# ExampleGet your own Python Server
+# Loop through the set, and print the values:
+
+thisset = {"apple", "banana", "cherry"}
+
+for x in thisset:
+  print(x)
+
+# $ python3 loop-items.py 
+# banana
+# apple
+# cherry
