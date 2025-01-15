cdff74f68a577ac35e2e04fd145240080aafca2e
David Stevenson <drstevenson1958@yahoo.com>
2025-01-15 17:08:30 -0500

    W3Schools: 016-Python-Dictionaries: Copy Dictionaries

---
+++ W3Schools/016-Python-Dictionaries/7-Copy-Dictionaries/copy-dictionary-using-dict-constructor.py
@@ -0,0 +1,13 @@
+# Example
+# Make a copy of a dictionary with the dict() function:
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+mydict = dict(thisdict)
+print(mydict)
+
+# $ python3 copy-dictionary-using-dict-constructor.py 
+# {'brand': 'Ford', 'model': 'Mustang', 'year': 1964}
