f6274f6a7ebba4e9763ffb1f0adc51929e4003b9
David Stevenson <drstevenson1958@yahoo.com>
2025-01-15 17:16:28 -0500

    W3Schools: 016-Python-Dictionaries: Nested Dictionaries

---
+++ W3Schools/016-Python-Dictionaries/8-Nested-Dictionaries/access-items.py
@@ -0,0 +1,27 @@
+
+
+# Access Items in Nested Dictionaries
+# To access items from a nested dictionary, you use the name of the dictionaries, starting with the outer dictionary:
+
+# Example
+# Print the name of child 2:
+
+myfamily = {
+  "child1" : {
+    "name" : "Emil",
+    "year" : 2004
+  },
+  "child2" : {
+    "name" : "Tobias",
+    "year" : 2007
+  },
+  "child3" : {
+    "name" : "Linus",
+    "year" : 2011
+  }
+}
+
+print(myfamily["child2"]["name"])
+
+# $ python3 access-items.py 
+# Tobias
