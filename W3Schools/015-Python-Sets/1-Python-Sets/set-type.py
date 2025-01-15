62165b6ad6188aec0703128ffe6ac4187ff6a25b
David Stevenson <drstevenson1958@yahoo.com>
2025-01-15 12:45:40 -0500

    W3Schools: 015-Python-Sets: sets are defined as objects with the data type 'set'

---
+++ W3Schools/015-Python-Sets/1-Python-Sets/set-type.py
@@ -0,0 +1,12 @@
+# type()
+# From Python's perspective, sets are defined as objects with the data type 'set':
+
+# <class 'set'>
+# Example
+# What is the data type of a set?
+
+myset = {"apple", "banana", "cherry"}
+print(type(myset))
+
+# $ python3 set-type.py
+# <class 'set'>
