a84cb66e5d3d08361cc16c979416df0e0758b27c
David Stevenson <drstevenson1958@yahoo.com>
2025-01-15 12:47:10 -0500

    W3Schools: 015-Python-Sets: Using the set() constructor to make a set

---
+++ W3Schools/015-Python-Sets/1-Python-Sets/set-constructor.py
@@ -0,0 +1,11 @@
+# The set() Constructor
+# It is also possible to use the set() constructor to make a set.
+
+# Example
+# Using the set() constructor to make a set:
+
+thisset = set(("apple", "banana", "cherry")) # note the double round-brackets
+print(thisset)
+
+# $ python3 set-constructor.py 
+# {'apple', 'cherry', 'banana'}
