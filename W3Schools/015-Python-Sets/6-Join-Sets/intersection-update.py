a8a4911b63cbf3f73c92cb02e4ce16281353dfc6
David Stevenson <drstevenson1958@yahoo.com>
2025-01-15 13:54:28 -0500

    W3Schools: 015-Python-Sets: Join Sets: The intersection_update() method will also keep ONLY the duplicates, but it will change the original set instead of returning a new set.

---
+++ W3Schools/015-Python-Sets/6-Join-Sets/intersection-update.py
@@ -0,0 +1,15 @@
+# The intersection_update() method will also keep ONLY the duplicates, 
+# but it will change the original set instead of returning a new set.
+
+# Example
+# Keep the items that exist in both set1, and set2:
+
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+
+set1.intersection_update(set2)
+
+print(set1)
+
+# $ python3 intersection-update.py 
+# {'apple'}
