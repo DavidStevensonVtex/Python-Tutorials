08c0165f9fc8ca53de2a6800102d08490cd13001
David Stevenson <drstevenson1958@yahoo.com>
2025-01-15 12:39:29 -0500

    W3Schools: 015-Python-Sets: Get the Length of a Set

---
+++ W3Schools/015-Python-Sets/1-Python-Sets/set-length.py
@@ -0,0 +1,11 @@
+# Get the Length of a Set
+# To determine how many items a set has, use the len() function.
+
+# Example
+# Get the number of items in a set:
+
+thisset = {"apple", "banana", "cherry"}
+print(len(thisset))
+
+# $ python3 set-length.py 
+# 3
