aa63bc1c3e74908c28ee0ec955bacdb54d05a783
David Stevenson <drstevenson1958@yahoo.com>
2025-01-15 13:09:24 -0500

    W3Schools: 015-Python-Sets: Remove Item - To remove an item in a set, use the remove(), or the discard() method.

---
+++ W3Schools/015-Python-Sets/4-Remove-Set-Items/remove-item.py
@@ -0,0 +1,13 @@
+# Remove Item
+# To remove an item in a set, use the remove(), or the discard() method.
+
+# Remove "banana" by using the remove() method:
+
+thisset = {"apple", "banana", "cherry"}
+
+thisset.remove("banana")
+
+print(thisset)
+
+# $ python3 remove-item.py 
+# {'apple', 'cherry'}
