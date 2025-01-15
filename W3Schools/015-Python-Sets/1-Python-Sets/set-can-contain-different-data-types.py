2c7d92d70b6b6e74bf640906563addc62650c219
David Stevenson <drstevenson1958@yahoo.com>
2025-01-15 12:42:33 -0500

    W3Schools: 015-Python-Sets: A set can contain different data types

---
+++ W3Schools/015-Python-Sets/1-Python-Sets/set-can-contain-different-data-types.py
@@ -0,0 +1,10 @@
+# A set can contain different data types:
+
+# Example
+# A set with strings, integers and boolean values:
+
+set1 = {"abc", 34, True, 40, "male"}
+print(set1)
+
+# $ python3 set-can-contain-different-data-types.py 
+# {True, 34, 40, 'abc', 'male'}
