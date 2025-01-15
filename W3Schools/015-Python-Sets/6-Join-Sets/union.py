ff0009a18440d1a6268d6d9fee181232f5c13661
David Stevenson <drstevenson1958@yahoo.com>
2025-01-15 13:32:52 -0500

    W3Schools: 015-Python-Sets: Join Sets - The union() method returns a new set with all items from both sets.

---
+++ W3Schools/015-Python-Sets/6-Join-Sets/union.py
@@ -0,0 +1,13 @@
+# Union
+# The union() method returns a new set with all items from both sets.
+
+# Join set1 and set2 into a new set:
+
+set1 = {"a", "b", "c"}
+set2 = {1, 2, 3}
+
+set3 = set1.union(set2)
+print(set3)
+
+# $ python3 union.py 
+# {1, 2, 3, 'b', 'a', 'c'}
