81a091057a436631a40594ec495138757c62ab38
David Stevenson <drstevenson1958@yahoo.com>
2025-01-15 13:00:51 -0500

    W3Schools: 015-Python-Sets: Add Items - To add one item to a set use the add() method

---
+++ W3Schools/015-Python-Sets/3-Add-Set-Items/add-items.py
@@ -0,0 +1,14 @@
+# Add Items
+# Once a set is created, you cannot change its items, but you can add new items.
+
+# To add one item to a set use the add() method.
+
+# ExampleGet your own Python Server
+# Add an item to a set, using the add() method:
+
+thisset = {"apple", "banana", "cherry"}
+thisset.add("orange")
+print(thisset)
+
+# $ python3 add-items.py 
+# {'cherry', 'orange', 'apple', 'banana'}
