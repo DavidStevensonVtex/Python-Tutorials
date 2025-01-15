dc72eab29992396a8bf8899e05aed9e30b8de015
David Stevenson <drstevenson1958@yahoo.com>
2025-01-15 13:57:04 -0500

    W3Schools: 015-Python-Sets: Join Sets: Difference
    The difference() method will return a new set that will contain only the items from the first set that are not present in the other set.

---
+++ W3Schools/015-Python-Sets/6-Join-Sets/difference.py
@@ -0,0 +1,15 @@
+# Difference
+# The difference() method will return a new set that will contain only the items 
+# from the first set that are not present in the other set.
+
+# Example
+# Keep all items from set1 that are not in set2:
+
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+
+set3 = set1.difference(set2)
+print(set3)
+
+# $ python3 difference.py 
+# {'cherry', 'banana'}
