7131a9ce450abc939a4345de1cbdadbd2e84372e
David Stevenson <drstevenson1958@yahoo.com>
2025-01-15 12:56:40 -0500

    W3Schools: 015-Python-Sets: Check if value is not present in the set

---
+++ W3Schools/015-Python-Sets/2-Access-Set-Items/check-if-value-is-not-present.py
@@ -0,0 +1,8 @@
+# Example
+# Check if "banana" is NOT present in the set:
+
+thisset = {"apple", "banana", "cherry"}
+print("banana" not in thisset)
+
+# $ python3 check-if-value-is-not-present.py 
+# False
