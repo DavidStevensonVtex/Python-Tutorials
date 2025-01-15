8a09705a5f8b5c660b408db16b333793094aea38
David Stevenson <drstevenson1958@yahoo.com>
2025-01-15 16:29:30 -0500

    W3Schools: 016-Python-Dictionaries: Change Items

---
+++ W3Schools/016-Python-Dictionaries/3-Change-Items/change-values.py
@@ -0,0 +1,15 @@
+# Change Values
+# You can change the value of a specific item by referring to its key name:
+
+# Change the "year" to 2018:
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict["year"] = 2018
+print(thisdict)
+
+# $ python3 change-values.py 
+# {'brand': 'Ford', 'model': 'Mustang', 'year': 2018}
