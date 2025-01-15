359d6d8eb9cb3b86c4f802e0376ada69efb456f8
David Stevenson <drstevenson1958@yahoo.com>
2025-01-15 12:40:30 -0500

    W3Schools: 015-Python-Sets: Set items can be of any data type

---
+++ W3Schools/015-Python-Sets/1-Python-Sets/set-item-data-types.py
@@ -0,0 +1,9 @@
+# Set Items - Data Types
+# Set items can be of any data type:
+
+# Example
+# String, int and boolean data types:
+
+set1 = {"apple", "banana", "cherry"}
+set2 = {1, 5, 7, 9, 3}
+set3 = {True, False, False}
