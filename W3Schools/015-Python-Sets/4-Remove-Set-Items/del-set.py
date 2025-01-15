a66cb0f1ba989d89d308aca2fbc90c9390752c75
David Stevenson <drstevenson1958@yahoo.com>
2025-01-15 13:20:04 -0500

    W3Schools: 015-Python-Sets: Delete Set - The del keyword will delete the set completely

---
+++ W3Schools/015-Python-Sets/4-Remove-Set-Items/del-set.py
@@ -0,0 +1,14 @@
+# Example
+# The del keyword will delete the set completely:
+
+thisset = {"apple", "banana", "cherry"}
+
+del thisset
+
+print(thisset)
+
+# $ python3 del-set.py 
+# Traceback (most recent call last):
+#   File "del-set.py", line 8, in <module>
+#     print(thisset)
+# NameError: name 'thisset' is not defined
