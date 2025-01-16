0d7aee87c30f4f4301ad3fcba75da75a1e7d4395
David Stevenson <drstevenson1958@yahoo.com>
2025-01-16 11:50:32 -0500

    Chapter 7: Input and Output: Copy Standard Input to Standard Output

---
+++ Python-Projects/The-C-Programming-Language/ch07/copy-input-to-output.py
@@ -0,0 +1,10 @@
+import sys
+
+while c := sys.stdin.read(1):
+    sys.stdout.write(c)
+
+# $ python3 copy-input-to-output.py < rain-in-Spain.txt 
+# The rain in Spain falls mainly in the plain.
+
+# It's tough to make predictions, especially about the future.
+# -- Yogi Berra
