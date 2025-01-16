c7ac9b400ad24fc485bcccdc569756a2e58d9466
David Stevenson <drstevenson1958@yahoo.com>
2025-01-16 10:06:10 -0500

    W3Schools: 017-Python-If-Else: You can also have multiple else statements on the same line
    Example
    
    One line if else statement, with 3 conditions

---
+++ W3Schools/017-Python-If-Else/multiple-else-statements-on-same-line.py
@@ -0,0 +1,11 @@
+# You can also have multiple else statements on the same line:
+
+# Example
+# One line if else statement, with 3 conditions:
+
+a = 330
+b = 330
+print("A") if a > b else print("=") if a == b else print("B")
+
+# $ python3 multiple-else-statements-on-same-line.py 
+# =
