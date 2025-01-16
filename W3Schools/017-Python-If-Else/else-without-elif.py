9f83ebe0ac6679d6108be4923f839be157fa6538
David Stevenson <drstevenson1958@yahoo.com>
2025-01-16 09:56:11 -0500

    W3Schools: 017-Python-If-Else: else without elif

---
+++ W3Schools/017-Python-If-Else/else-without-elif.py
@@ -0,0 +1,12 @@
+# You can also have an else without the elif:
+
+# Example
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+# $ python3 else-without-elif.py 
+# b is not greater than a
