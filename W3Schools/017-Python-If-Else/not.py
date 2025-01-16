c5ff45cae5c3c7c4538291cb8fc1ca9e6842eb7d
David Stevenson <drstevenson1958@yahoo.com>
2025-01-16 10:11:15 -0500

    W3Schools: 017-Python-If-Else: The not keyword is a logical operator, and is used to reverse the result of the conditional statement

---
+++ W3Schools/017-Python-If-Else/not.py
@@ -0,0 +1,13 @@
+# Not
+# The not keyword is a logical operator, and is used to reverse the result of the conditional statement:
+
+# Example
+# Test if a is NOT greater than b:
+
+a = 33
+b = 200
+if not a > b:
+  print("a is NOT greater than b")
+
+# $ python3 not.py 
+# a is NOT greater than b
