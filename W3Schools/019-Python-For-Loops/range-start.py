31f08a90d940cd1861f528a817a632a621ad0b46
David Stevenson <drstevenson1958@yahoo.com>
2025-01-16 10:44:48 -0500

    W3Schools: 019-Python-For-Loop: The range() function defaults to 0 as a starting value, however it is possible to specify the starting value by adding a parameter: range(2, 6), which means values from 2 to 6 (but not including 6)

---
+++ W3Schools/019-Python-For-Loops/range-start.py
@@ -0,0 +1,15 @@
+# The range() function defaults to 0 as a starting value, however it is possible to specify 
+# the starting value by adding a parameter: range(2, 6), which means values from 2 to 6 
+# (but not including 6):
+
+# Example
+# Using the start parameter:
+
+for x in range(2, 6):
+  print(x)
+
+# $ python3 range-start.py 
+# 2
+# 3
+# 4
+# 5
