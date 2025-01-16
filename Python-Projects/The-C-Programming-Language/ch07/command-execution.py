a5ff3e36e9c3f6d1cbae1c7ecde09cae72cfa0ec
David Stevenson <drstevenson1958@yahoo.com>
2025-01-16 14:39:25 -0500

    Chapter 7: Input and Output: Command Execution

---
+++ Python-Projects/The-C-Programming-Language/ch07/command-execution.py
@@ -0,0 +1,19 @@
+import subprocess
+
+subprocess.run(["ls", "-l"]) 
+
+# $ python3 command-execution.py 
+# total 52
+# -rw-rw-r-- 1 dstevenson dstevenson   48 Jan 16 14:38 command-execution.py
+# -rw-rw-r-- 1 dstevenson dstevenson  250 Jan 16 11:50 copy-input-to-output.py
+# -rw-rw-r-- 3 dstevenson dstevenson  691 Jan 16 12:01 exercise-7-01.py
+# -rw-rw-r-- 1 dstevenson dstevenson 1721 Jan 16 12:43 exercise-7-02.py
+# -rw-rw-r-- 1 dstevenson dstevenson 1392 Jan 16 14:30 exercise-7-06.py
+# -rwxr--r-- 1 dstevenson dstevenson  195 Jan 16 14:28 file1.txt
+# -rwxr--r-- 1 dstevenson dstevenson  180 Jan 16 14:28 file2.txt
+# -rw-rw-r-- 3 dstevenson dstevenson  691 Jan 16 12:01 lower.py
+# -rw-rw-r-- 1 dstevenson dstevenson 1014 Jan 16 12:57 minprintf.py
+# -rw-rw-r-- 1 dstevenson dstevenson  333 Jan 16 12:07 printf-hello-world.py
+# -rw-rw-r-- 1 dstevenson dstevenson  146 Jan 16 12:30 rain-in-Spain.txt
+# -rw-rw-r-- 1 dstevenson dstevenson 3424 Jan 16 14:36 README.md
+# -rw-rw-r-- 3 dstevenson dstevenson  691 Jan 16 12:01 upper.py
