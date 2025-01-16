cfb55a6d91ac6e9ea9c64e064bb99a25482a3550
David Stevenson <drstevenson1958@yahoo.com>
2025-01-16 09:54:19 -0500

    W3Schools: 017-Python-If-Else: Else

---
+++ W3Schools/017-Python-If-Else/else.py
@@ -0,0 +1,15 @@
+# Else
+# The else keyword catches anything which isn't caught by the preceding conditions.
+
+# Example
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
+
+# $ python3 else.py
+# a is greater than b
