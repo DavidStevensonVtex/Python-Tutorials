8c32bc3409196f22569e97ecda7cde445b6025fc
David Stevenson <drstevenson1958@yahoo.com>
2025-01-16 10:35:55 -0500

    W3Schools: 019-Python-For-Loops: Looping Through a String
    Even strings are iterable objects, they contain a sequence of characters

---
+++ W3Schools/019-Python-For-Loops/looping-thru-a-string.py
@@ -0,0 +1,16 @@
+# Looping Through a String
+# Even strings are iterable objects, they contain a sequence of characters:
+
+# Example
+# Loop through the letters in the word "banana":
+
+for x in "banana":
+  print(x)
+
+# $ python3 looping-thru-a-string.py 
+# b
+# a
+# n
+# a
+# n
+# a
