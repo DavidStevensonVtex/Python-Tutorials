8c52682f54aace9f2d447f8ccaf136a85d8ac0a2
David Stevenson <drstevenson1958@yahoo.com>
2025-01-16 10:49:08 -0500

    W3Schools: 019-Python-For-Loops: The range() function defaults to increment the sequence by 1, however it is possible to specify the increment value by adding a third parameter: range(2, 30, 3)

---
+++ W3Schools/019-Python-For-Loops/range-start-step.py
@@ -0,0 +1,20 @@
+# The range() function defaults to increment the sequence by 1, however it is possible to 
+# specify the increment value by adding a third parameter: range(2, 30, 3):
+
+# Example
+# Increment the sequence with 3 (default is 1):
+
+for x in range(2, 30, 3):
+  print(x)
+
+# $ python3 range-start-step.py 
+# 2
+# 5
+# 8
+# 11
+# 14
+# 17
+# 20
+# 23
+# 26
+# 29
