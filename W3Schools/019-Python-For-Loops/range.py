036337dd5ee06ad9820ffd1556e60b299485622d
David Stevenson <drstevenson1958@yahoo.com>
2025-01-16 10:42:16 -0500

    W3Schools: 019-Python-For-Loop: The range() Function
    To loop through a set of code a specified number of times, we can use the range() function,
    
    The range() function returns a sequence of numbers, starting from 0 by default, and increments by 1 (by default), and ends before the specified number.

---
+++ W3Schools/019-Python-For-Loops/range.py
@@ -0,0 +1,20 @@
+# The range() Function
+# To loop through a set of code a specified number of times, we can use the range() function,
+# The range() function returns a sequence of numbers, starting from 0 by default, and increments 
+# by 1 (by default), and ends at a specified number.
+
+# Example
+# Using the range() function:
+
+for x in range(6):
+  print(x)
+
+# Note that range(6) is not the values of 0 to 6, but the values 0 to 5.
+
+# $ python3 range.py 
+# 0
+# 1
+# 2
+# 3
+# 4
+# 5
