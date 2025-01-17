c242cc4831237bb642445209e588bc241819f09a
David Stevenson <drstevenson1958@yahoo.com>
2025-01-17 10:21:29 -0500

    W3Schools: 022-Python-Arrays: Removing Array Elements
    You can use the pop() method to remove an element from the array.

---
+++ W3Schools/022-Python-Arrays/remove-element.py
@@ -0,0 +1,14 @@
+# Removing Array Elements
+# You can use the pop() method to remove an element from the array.
+
+# Example
+# Delete the second element of the cars array:
+
+cars = ["Ford", "Volvo", "BMW"]
+print("before pop", cars)
+cars.pop(1)
+print("after pop(1)", cars)
+
+# $ python3 remove-element.py 
+# before pop ['Ford', 'Volvo', 'BMW']
+# after pop(1) ['Ford', 'BMW']
