9edf94c3ee4e47bf88cb601ceb0a28cba036c759
David Stevenson <drstevenson1958@yahoo.com>
2025-01-17 09:51:58 -0500

    W3Schools: 021-Python-Lambda: Lambda functions can take any number of arguments

---
+++ W3Schools/021-Python-Lambda/lambda-2.py
@@ -0,0 +1,10 @@
+# Lambda functions can take any number of arguments:
+
+# Example
+# Multiply argument a with argument b and return the result:
+
+x = lambda a, b : a * b
+print(x(5, 6))
+
+# $ python3 lambda-2.py 
+# 30
