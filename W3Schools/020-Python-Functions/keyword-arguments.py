187cc621ad742f5817d35624e35185c82a26c5ab
David Stevenson <drstevenson1958@yahoo.com>
2025-01-17 09:16:55 -0500

    W3Schools: 020-Python-Functions: Keyword Arguments
    You can also send arguments with the key = value syntax.
    
    This way the order of the arguments does not matter.

---
+++ W3Schools/020-Python-Functions/keyword-arguments.py
@@ -0,0 +1,14 @@
+# Keyword Arguments
+
+# You can also send arguments with the key = value syntax.
+
+# This way the order of the arguments does not matter.
+
+# Example
+def my_function(child3, child2, child1):
+  print("The youngest child is " + child3)
+
+my_function(child1 = "Emil", child2 = "Tobias", child3 = "Linus")
+
+# $ python3 keyword-arguments.py 
+# The youngest child is Linus
