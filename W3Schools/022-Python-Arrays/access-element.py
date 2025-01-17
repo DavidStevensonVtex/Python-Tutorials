e18cbdc797e0444a855b44de9f15e838585134dc
David Stevenson <drstevenson1958@yahoo.com>
2025-01-17 10:13:02 -0500

    W3Schools: 022-Python-Arrays: Access the Elements of an Array
    You refer to an array element by referring to the index number.

---
+++ W3Schools/022-Python-Arrays/access-element.py
@@ -0,0 +1,21 @@
+# Access the Elements of an Array
+# You refer to an array element by referring to the index number.
+
+# Example
+# Get the value of the first array item:
+
+cars = ["Ford", "Volvo", "BMW"]
+print(cars)
+
+x = cars[0]
+print(x)
+
+# Example
+# Modify the value of the first array item:
+
+cars[0] = "Toyota"
+print(cars)
+
+# ['Ford', 'Volvo', 'BMW']
+# Ford
+# ['Toyota', 'Volvo', 'BMW']
