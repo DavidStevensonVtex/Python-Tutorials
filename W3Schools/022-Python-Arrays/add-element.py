16706d06db8e35bc7df3e4327c9a53fec7b21611
David Stevenson <drstevenson1958@yahoo.com>
2025-01-17 10:18:50 -0500

    W3Schools: 022-Python-Arrays: Adding Array Elements
    You can use the append() method to add an element to an array.

---
+++ W3Schools/022-Python-Arrays/add-element.py
@@ -0,0 +1,14 @@
+# Adding Array Elements
+# You can use the append() method to add an element to an array.
+
+# Example
+# Add one more element to the cars array:
+
+cars = ["Ford", "Volvo", "BMW"]
+print("before append", cars)
+cars.append("Honda")
+print("after append", cars)
+
+# $ python3 add-element.py 
+# before append ['Ford', 'Volvo', 'BMW']
+# after append ['Ford', 'Volvo', 'BMW', 'Honda']
