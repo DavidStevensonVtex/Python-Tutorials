e7f7edd346c452b5ae2d4a8213059a354b42c598
David Stevenson <drstevenson1958@yahoo.com>
2025-01-18 15:02:46 -0500

    W3Schools: 023-Python-Classes-and-Objects: Create Object
    Now we can use the class named MyClass to create objects:
    
    Create an object named p1, and print the value of x

--- W3Schools/023-Python-Classes-and-Objects/class-1.py
+++ W3Schools/023-Python-Classes-and-Objects/class-1.py
@@ -12,3 +12,15 @@
 
 class MyClass:
   x = 5
+
+# Create Object
+# Now we can use the class named MyClass to create objects:
+
+# Example
+# Create an object named p1, and print the value of x:
+
+p1 = MyClass()
+print(p1.x)
+
+$ python3 class-1.py 
+5
