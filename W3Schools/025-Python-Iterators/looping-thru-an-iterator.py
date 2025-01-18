7ec2bc3b6f27411e8f9380f416d5dc1f545d6b7c
David Stevenson <drstevenson1958@yahoo.com>
2025-01-18 18:05:30 -0500

    W3Schools: 025-Python-Iterators: Looping Through an Iterator
    We can also use a for loop to iterate through an iterable object

---
+++ W3Schools/025-Python-Iterators/looping-thru-an-iterator.py
@@ -0,0 +1,16 @@
+# Looping Through an Iterator
+
+# We can also use a for loop to iterate through an iterable object:
+
+# Example
+# Iterate the values of a tuple:
+
+mytuple = ("apple", "banana", "cherry")
+
+for x in mytuple:
+  print(x)
+
+# $ python3 looping-thru-an-iterator.py 
+# apple
+# banana
+# cherry
