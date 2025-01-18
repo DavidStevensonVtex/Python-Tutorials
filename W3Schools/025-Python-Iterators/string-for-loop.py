6d9f6cb3cf8605e9c7561448114b5f1c5d763da6
David Stevenson <drstevenson1958@yahoo.com>
2025-01-18 18:12:07 -0500

    W3Schools: 025-Python-Iterators: Create an Iterator
    To create an object/class as an iterator you have to implement the methods __iter__() and __next__() to your object.

--- W3Schools/025-Python-Iterators/string-for-loop.py
+++ W3Schools/025-Python-Iterators/string-for-loop.py
@@ -5,6 +5,8 @@ mystr = "banana"
 for x in mystr:
   print(x)
 
+# The for loop actually creates an iterator object and executes the next() method for each loop.
+
 # $ python3 string-for-loop.py 
 # b
 # a
