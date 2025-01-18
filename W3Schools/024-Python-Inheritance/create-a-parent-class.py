ce09a2e0cc8b25ba72c2c9821b77df05fcbd82c6
David Stevenson <drstevenson1958@yahoo.com>
2025-01-18 15:45:13 -0500

    W3Schools: 024-Python-Inheritance: add a call # to the parent's __init__() function
    Person.__init__(self, fname, lname)

--- W3Schools/024-Python-Inheritance/create-a-parent-class.py
+++ W3Schools/024-Python-Inheritance/create-a-parent-class.py
@@ -36,10 +36,19 @@ x.printname()
 # Example
 # Add the __init__() function to the Student class:
 
+# When you add the __init__() function, the child class will no longer inherit the 
+# parent's __init__() function.
+
+# Note: The child's __init__() function overrides the inheritance of the 
+# parent's __init__() function.
+
+# To keep the inheritance of the parent's __init__() function, add a call 
+# to the parent's __init__() function:
+
+# Example
 class Student(Person):
   def __init__(self, fname, lname):
-    self.firstname = fname
-    self.lastname = lname
+    Person.__init__(self, fname, lname)
 
 s = Student("Sam", "Spade")
 s.printname()
