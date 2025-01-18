3f9617bff7df97b9e9d3c49de6c0b134228dd1bf
David Stevenson <drstevenson1958@yahoo.com>
2025-01-18 15:30:59 -0500

    W3Schools: 023-Python-Classes-and-Objects: The pass Statement
    class definitions cannot be empty, but if you for some reason have a class definition with no content, put in the pass statement to avoid getting an error.

---
+++ W3Schools/023-Python-Classes-and-Objects/class-pass.py
@@ -0,0 +1,8 @@
+# The pass Statement
+
+# class definitions cannot be empty, but if you for some reason have a class definition 
+# with no content, put in the pass statement to avoid getting an error.
+
+# Example
+class Person:
+  pass
