c62aac800d9d3e5e74d92d578813078994d55120
David Stevenson <drstevenson1958@yahoo.com>
2025-01-19 08:59:22 -0500

    W3Schools: 028-Python-Modules: Selective import
    Import only the person1 dictionary from the module

---
+++ W3Schools/028-Python-Modules/selective-import.py
@@ -0,0 +1,10 @@
+# Example
+
+# Import only the person1 dictionary from the module:
+
+from mymodule import person1
+
+print (person1["age"])
+
+# $ python3 selective-import.py 
+# 36
--- W3Schools/028-Python-Modules/using-the-dir-function.py
+++ W3Schools/028-Python-Modules/using-the-dir-function.py
@@ -12,5 +12,7 @@ import platform
 x = dir(platform)
 print(x)
 
+# Note: The dir() function can be used on all modules, also the ones you create yourself.
+
 # $ python3 using-the-dir-function.py 
 # ['_WIN32_CLIENT_RELEASES', '_WIN32_SERVER_RELEASES', '__builtins__', '__cached__', '__copyright__', '__doc__', '__file__', '__loader__', '__name__', '__package__', '__spec__', '__version__', '_comparable_version', '_component_re', '_default_architecture', '_follow_symlinks', '_ironpython26_sys_version_parser', '_ironpython_sys_version_parser', '_java_getprop', '_libc_search', '_mac_ver_xml', '_node', '_norm_version', '_platform', '_platform_cache', '_pypy_sys_version_parser', '_sys_version', '_sys_version_cache', '_sys_version_parser', '_syscmd_file', '_syscmd_uname', '_syscmd_ver', '_uname_cache', '_ver_output', '_ver_stages', 'architecture', 'collections', 'java_ver', 'libc_ver', 'mac_ver', 'machine', 'node', 'os', 'platform', 'processor', 'python_branch', 'python_build', 'python_compiler', 'python_implementation', 'python_revision', 'python_version', 'python_version_tuple', 're', 'release', 'sys', 'system', 'system_alias', 'uname', 'uname_result', 'version', 'win32_edition', 'win32_is_iot', 'win32_ver']
