19599167f8a871bb1efacaabc72dbc8f55c38f39
David Stevenson <drstevenson1958@yahoo.com>
2025-01-19 08:55:14 -0500

    W3Schools: 028-Python-Modules: Built-in Modules
    There are several built-in modules in Python, which you can import whenever you like.

---
+++ W3Schools/028-Python-Modules/built-in-modules.py
@@ -0,0 +1,15 @@
+# Built-in Modules
+
+# There are several built-in modules in Python, which you can import whenever you like.
+
+# Example
+
+# Import and use the platform module:
+
+import platform
+
+x = platform.system()
+print(x)
+
+# $ python3 built-in-modules.py 
+# Linux
