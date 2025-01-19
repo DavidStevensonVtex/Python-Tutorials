c9444fae24c227585cdbe3b6f120443eed37ef27
David Stevenson <drstevenson1958@yahoo.com>
2025-01-19 08:48:15 -0500

    W3Schools: 028-Python-Modules: Use a Module
    Now we can use the module we just created, by using the import statement

---
+++ W3Schools/028-Python-Modules/use-a-module.py
@@ -0,0 +1,14 @@
+# Use a Module
+
+# Now we can use the module we just created, by using the import statement:
+
+# Example
+
+# Import the module named mymodule, and call the greeting function:
+
+import mymodule
+
+mymodule.greeting("Jonathan")
+
+# $ python3 use-a-module.py 
+# Hello, Jonathan
