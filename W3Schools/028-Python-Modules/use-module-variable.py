b92fdf2a21b5675113c43f9cf7c5ab2fcb0f7258
David Stevenson <drstevenson1958@yahoo.com>
2025-01-19 08:51:33 -0500

    W3Schools: 028-Python-Modules: Use a module variable
    Import the module named mymodule, and access the person1 dictionary

---
+++ W3Schools/028-Python-Modules/use-module-variable.py
@@ -0,0 +1,11 @@
+# Example
+
+# Import the module named mymodule, and access the person1 dictionary:
+
+import mymodule
+
+a = mymodule.person1["age"]
+print(a)
+
+# $ python3 use-module-variable.py 
+# 36
