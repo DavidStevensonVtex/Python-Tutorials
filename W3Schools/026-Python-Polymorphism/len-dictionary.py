b9fa15a04aaf148f5ccf50a7ef3b267c56202c86
David Stevenson <drstevenson1958@yahoo.com>
2025-01-19 08:17:38 -0500

    W3Schools: 026-Python-Polymorphism: dictionary len

---
+++ W3Schools/026-Python-Polymorphism/len-dictionary.py
@@ -0,0 +1,16 @@
+# Dictionary
+
+# For dictionaries len() returns the number of key/value pairs in the dictionary:
+
+# Example
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+print(len(thisdict))
+
+# $ python3 len-dictionary.py 
+# 3
