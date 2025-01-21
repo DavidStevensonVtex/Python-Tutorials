2c1beb0d869284e8628d5aedbac2ed39020c06ad
David Stevenson <drstevenson1958@yahoo.com>
2025-01-21 09:51:18 -0500

    W3Schools: 030-Python-Math: Python Math
    Python has a set of built-in math functions, including an extensive math module, that allows you to perform mathematical tasks on numbers.
    
    Built-in Math Functions
    
    The min() and max() functions can be used to find the lowest or highest value in an iterable:

---
+++ W3Schools/030-Python-Math/min-and-max.py
@@ -0,0 +1,19 @@
+# Python Math
+
+# Python has a set of built-in math functions, including an extensive math module, 
+# that allows you to perform mathematical tasks on numbers.
+
+# Built-in Math Functions
+
+# The min() and max() functions can be used to find the lowest or highest value in 
+# an iterable:
+
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+
+print(x)
+print(y)
+
+# $ python3 min-and-max.py 
+# 5
+# 25
