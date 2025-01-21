ca9757bfa01abf8403a00ee0e67ecfe7c4dcb61a
David Stevenson <drstevenson1958@yahoo.com>
2025-01-21 09:21:04 -0500

    W3Schools: 029-Python-Dates

---
+++ W3Schools/029-Python-Dates/datetime-now.py
@@ -0,0 +1,16 @@
+# Python datetime
+
+# Python Dates
+
+# A date in Python is not a data type of its own, but we can import a module named 
+# datetime to work with dates as date objects.
+
+# Import the datetime module and display the current date:
+
+import datetime
+
+x = datetime.datetime.now()
+print(x)
+
+# $ python3 datetime-now.py 
+# 2025-01-21 09:19:55.522254
