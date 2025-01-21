9ea79c0bdf94a706d7eaf9f0e27baffc5ea42d98
David Stevenson <drstevenson1958@yahoo.com>
2025-01-21 09:33:35 -0500

    W3Schools: 029-Python-Dates: The strftime() Method
    The datetime object has a method for formatting date objects into readable strings.
    
    The method is called strftime(), and takes one parameter, format, to specify the format of the returned string

--- W3Schools/029-Python-Dates/creating-date-objects.py
+++ W3Schools/029-Python-Dates/creating-date-objects.py
@@ -14,5 +14,10 @@ x = datetime.datetime(2020, 5, 17)
 
 print(x)
 
+# The datetime() class also takes parameters for time and timezone 
+# (hour, minute, second, microsecond, tzone), but they are optional, 
+# and has a default value of 0, (None for timezone).
+
+
 # $ python3 creating-date-objects.py 
 # 2020-05-17 00:00:00
