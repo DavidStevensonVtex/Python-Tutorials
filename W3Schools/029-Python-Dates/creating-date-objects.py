24b185b659fb63be2d00ab8beec10e91f1af6403
David Stevenson <drstevenson1958@yahoo.com>
2025-01-21 09:29:16 -0500

    W3Schools: 029-Python-Dates: Creating Date Objects
    To create a date, we can use the datetime() class (constructor) of the datetime module.
    
    The datetime() class requires three parameters to create a date: year, month, day.

---
+++ W3Schools/029-Python-Dates/creating-date-objects.py
@@ -0,0 +1,18 @@
+# Creating Date Objects
+
+# To create a date, we can use the datetime() class (constructor) of the datetime module.
+
+# The datetime() class requires three parameters to create a date: year, month, day.
+
+# Example
+
+# Create a date object:
+
+import datetime
+
+x = datetime.datetime(2020, 5, 17)
+
+print(x)
+
+# $ python3 creating-date-objects.py 
+# 2020-05-17 00:00:00
