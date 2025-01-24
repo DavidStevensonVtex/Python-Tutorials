a64bbdee8432a41ccfb85fcc3e4877f764cb5c55
David Stevenson <drstevenson1958@yahoo.com>
2025-01-24 13:22:17 -0500

    W3Schools: 033-Python-PIP: Uninstall the package named "camelcase"

---
+++ W3Schools/033-Python-PIP/uninstall-package.py
@@ -0,0 +1,17 @@
+# Find Packages
+# Find more packages at https://pypi.org/.
+
+# Remove a Package
+# Use the uninstall command to remove a package:
+
+# Example
+# Uninstall the package named "camelcase":
+
+# $ pip uninstall camelcase
+# Found existing installation: camelcase 0.2
+# Uninstalling camelcase-0.2:
+#   Would remove:
+#     /home/dstevenson/.local/lib/python3.8/site-packages/camelcase-0.2.dist-info/*
+#     /home/dstevenson/.local/lib/python3.8/site-packages/camelcase/*
+# Proceed (y/n)? y
+#   Successfully uninstalled camelcase-0.2
