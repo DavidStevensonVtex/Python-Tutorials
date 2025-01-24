950916fbdf040a6b668b2e5d01827a67759ea125
David Stevenson <drstevenson1958@yahoo.com>
2025-01-24 13:14:23 -0500

    W3Schools: 033-Python-PIP: Python PIP
    What is PIP?
    
    PIP is a package manager for Python packages, or modules if you like.

---
+++ W3Schools/033-Python-PIP/check-if-pip-is-installed.py
@@ -0,0 +1,28 @@
+# Python PIP
+
+# What is PIP?
+
+# PIP is a package manager for Python packages, or modules if you like.
+
+# Note: If you have Python version 3.4 or later, PIP is included by default.
+
+# What is a Package?
+
+# A package contains all the files you need for a module.
+
+# Modules are Python code libraries you can include in your project.
+
+# Check if PIP is Installed
+
+# Navigate your command line to the location of Python's script directory, and type the following:
+
+# Check PIP version:
+
+# $ pip --version
+
+# Command 'pip' not found, but can be installed with:
+
+# sudo apt install python3-pip
+
+# $ pip --version
+# pip 20.0.2 from /usr/lib/python3/dist-packages/pip (python 3.8)
