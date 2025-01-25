ad50814a00b09c0c38f9a7bf1c2b1c5781e5e257
David Stevenson <drstevenson1958@yahoo.com>
2025-01-25 13:47:37 -0500

    W3Schools: 035-Python-User-Input: Python 2.7
    username = raw_input("Enter username:")

---
+++ W3Schools/Python-Tutorial/035-Python-User-Input/raw-input.py
@@ -0,0 +1,10 @@
+# Python 2.7
+
+username = raw_input("Enter username:")
+print("Username is: " + username)
+
+# $ python3 raw-input.py 
+# Traceback (most recent call last):
+#   File "raw-input.py", line 2, in <module>
+#     username = raw_input("Enter username:")
+# NameError: name 'raw_input' is not defined
