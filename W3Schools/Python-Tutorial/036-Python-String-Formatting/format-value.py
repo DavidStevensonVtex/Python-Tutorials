76a62fcfc46c4540d964c6ffeed0fa30acd82e5b
David Stevenson <drstevenson1958@yahoo.com>
2025-01-25 13:59:57 -0500

    W3Schools: 036-Python-String-Formatting: You can also format a value directly without keeping it in a variable

---
+++ W3Schools/Python-Tutorial/036-Python-String-Formatting/format-value.py
@@ -0,0 +1,11 @@
+# You can also format a value directly without keeping it in a variable:
+
+# Example
+
+# Display the value 95 with 2 decimals:
+
+txt = f"The price is {95:.2f} dollars"
+print(txt)
+
+# $ python3 format-value.py 
+# The price is 95.00 dollars
