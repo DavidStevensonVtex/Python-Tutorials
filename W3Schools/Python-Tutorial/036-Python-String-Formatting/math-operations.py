97a4881339b7abdc00b321ce336ddf36a3822e81
David Stevenson <drstevenson1958@yahoo.com>
2025-01-25 14:04:27 -0500

    W3Schools: 036-Python-String-Formatting: You can perform math operations on variables:
    Example
    
    Add taxes before displaying the price

---
+++ W3Schools/Python-Tutorial/036-Python-String-Formatting/math-operations.py
@@ -0,0 +1,13 @@
+# You can perform math operations on variables:
+
+# Example
+
+# Add taxes before displaying the price:
+
+price = 59
+tax = 0.25
+txt = f"The price is {price + (price * tax)} dollars"
+print(txt)
+
+# $ python3 math-operations.py 
+# The price is 73.75 dollars
