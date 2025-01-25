2cf0cdddb01de501ca3305fc4f22fcd8fe0c0562
David Stevenson <drstevenson1958@yahoo.com>
2025-01-25 14:02:35 -0500

    W3Schools: 036-Python-String-Formatting: Perform Operations in F-Strings
    You can perform Python operations inside the placeholders.
    
    You can do math operations

---
+++ W3Schools/Python-Tutorial/036-Python-String-Formatting/perform-operations.py
@@ -0,0 +1,15 @@
+# Perform Operations in F-Strings
+
+# You can perform Python operations inside the placeholders.
+
+# You can do math operations:
+
+# Example
+
+# Perform a math operation in the placeholder, and return the result:
+
+txt = f"The price is {20 * 59} dollars"
+print(txt)
+
+# $ python3 perform-operations.py 
+# The price is 1180 dollars
