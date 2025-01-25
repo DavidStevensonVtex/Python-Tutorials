283bf9f1354bd2897a7be196937ea842fd3ddb9d
David Stevenson <drstevenson1958@yahoo.com>
2025-01-25 15:46:23 -0500

    W3Schools: 032-Python-RegEx: Match Object
    A Match Object is an object containing information about the search and the result.

---
+++ W3Schools/Python-Tutorial/032-Python-RegEx/search-match-object.py
@@ -0,0 +1,16 @@
+# Match Object
+
+# A Match Object is an object containing information about the search and the result.
+
+# Note: If there is no match, the value None will be returned, instead of the Match Object.
+
+# Example
+
+# Do a search that will return a Match Object:
+
+import re
+
+txt = "The rain in Spain"
+x = re.search("ai", txt)
+print(x) #this will print an object
+
