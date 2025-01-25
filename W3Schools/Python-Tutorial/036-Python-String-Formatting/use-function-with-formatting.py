01264496c19a8d14e23c8b0c9651085d2450d2bc
David Stevenson <drstevenson1958@yahoo.com>
2025-01-25 14:08:55 -0500

    W3Schools: 036-Python-String-Formatting: Execute Functions in F-Strings
    You can execute functions inside the placeholder

---
+++ W3Schools/Python-Tutorial/036-Python-String-Formatting/use-function-with-formatting.py
@@ -0,0 +1,14 @@
+# Execute Functions in F-Strings
+
+# You can execute functions inside the placeholder:
+
+# Example
+
+# Use the string method upper()to convert a value into upper case letters:
+
+fruit = "apples"
+txt = f"I love {fruit.upper()}"
+print(txt)
+
+# $ python3 use-function-with-formatting.py 
+# I love APPLES
