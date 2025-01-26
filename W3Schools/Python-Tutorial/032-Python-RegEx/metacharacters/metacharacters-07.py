2abe2b6d3fda4560d2aede656713a448d93c05f1
David Stevenson <drstevenson1958@yahoo.com>
2025-01-26 13:56:30 -0500

    W3Schools: 032-Python-RegEx: metacharacters +:	One or more occurrences: example	"he.+o"

---
+++ W3Schools/Python-Tutorial/032-Python-RegEx/metacharacters/metacharacters-07.py
@@ -0,0 +1,12 @@
+import re
+
+txt = "hello planet"
+
+#Search for a sequence that starts with "he", followed by 1 or more  (any) characters, and an "o":
+
+x = re.findall("he.+o", txt)
+
+print(x)
+
+# $ python3 metacharacters-07.py
+# ['hello']
