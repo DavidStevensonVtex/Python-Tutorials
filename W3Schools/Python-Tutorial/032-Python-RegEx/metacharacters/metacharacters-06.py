4b359e87bb5221eeb444443b7e3697edf0d2c52b
David Stevenson <drstevenson1958@yahoo.com>
2025-01-26 13:51:04 -0500

    W3Schools: 032-Python-RegEx: metacharacters *: Zero or more occurrences: example "he.*o"

---
+++ W3Schools/Python-Tutorial/032-Python-RegEx/metacharacters/metacharacters-06.py
@@ -0,0 +1,11 @@
+import re
+
+txt = "hello planet"
+
+#Search for a sequence that starts with "he", followed by 0 or more  (any) characters, and an "o":
+
+x = re.findall("he.*o", txt)
+print(x)
+
+# $ python3 metacharacters-06.py
+# ['hello']
