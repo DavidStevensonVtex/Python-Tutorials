4b5ffdeaf37ae211d351e01af5b395c08379d689
David Stevenson <drstevenson1958@yahoo.com>
2025-01-27 09:41:09 -0500

    W3Schools: 032-Python-RegExp: special sequences \s:	Returns a match where the string contains a white space character: Example "\s"

---
+++ W3Schools/Python-Tutorial/032-Python-RegEx/special-sequences/special-sequences-08.py
@@ -0,0 +1,18 @@
+import re
+
+txt = "The rain in Spain"
+
+#Return a match at every white-space character:
+
+x = re.findall("\s", txt)
+
+print(x)
+
+if x:
+  print("Yes, there is at least one match!")
+else:
+  print("No match")
+
+# $ python3 special-sequences-08.py 
+# [' ', ' ', ' ']
+# Yes, there is at least one match!
