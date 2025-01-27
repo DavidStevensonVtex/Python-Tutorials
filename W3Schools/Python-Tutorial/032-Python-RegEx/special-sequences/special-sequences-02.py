63f9c90a091fdacf90e5ae2598bdaf51a94e4237
David Stevenson <drstevenson1958@yahoo.com>
2025-01-27 09:24:00 -0500

    W3Schools: 032-Python-RegExp: special sequences \b:	Returns a match where the specified characters are at the beginning or at the end of a word (the "r" in the beginning is making sure that the string is being treated as a "raw string"): Examples:	r"\bain", r"ain\b"

---
+++ W3Schools/Python-Tutorial/032-Python-RegEx/special-sequences/special-sequences-02.py
@@ -0,0 +1,18 @@
+import re
+
+txt = "The rain in Spain"
+
+#Check if "ain" is present at the beginning of a WORD:
+
+x = re.findall(r"\bain", txt)
+
+print(x)
+
+if x:
+  print("Yes, there is at least one match!")
+else:
+  print("No match")
+
+# $ python3 special-sequences-02.py
+# []
+# No match
