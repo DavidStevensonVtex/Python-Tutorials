13c7f5a0a371c907285d09abf191f39bf6f06671
David Stevenson <drstevenson1958@yahoo.com>
2025-01-27 09:31:54 -0500

    W3Schools: special sequences \B:	Returns a match where the specified characters are present, but NOT at the beginning (or at the end) of a word (the "r" in the beginning is making sure that the string is being treated as a "raw string") Example: r"ain\B"

---
+++ W3Schools/Python-Tutorial/032-Python-RegEx/special-sequences/special-sequences-05.py
@@ -0,0 +1,18 @@
+import re
+
+txt = "The rain in Spain"
+
+#Check if "ain" is present, but NOT at the end of a word:
+
+x = re.findall(r"ain\B", txt)
+
+print(x)
+
+if x:
+  print("Yes, there is at least one match!")
+else:
+  print("No match")
+
+# $ python3 special-sequences-05.py 
+# []
+# No match
