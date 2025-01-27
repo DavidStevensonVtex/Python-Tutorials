0fb47eb597f9920c9cbb0d0fd5c94a7abbce1504
David Stevenson <drstevenson1958@yahoo.com>
2025-01-27 09:46:21 -0500

    W3Schools: 032-Python-RegExp: special sequences \w:	Returns a match where the string contains any word characters (characters from a to Z, digits from 0-9, and the underscore _ character): Example: "\w"

---
+++ W3Schools/Python-Tutorial/032-Python-RegEx/special-sequences/special-sequences-10.py
@@ -0,0 +1,18 @@
+import re
+
+txt = "The rain in Spain"
+
+#Return a match at every word character (characters from a to Z, digits from 0-9, and the underscore _ character):
+
+x = re.findall("\w", txt)
+
+print(x)
+
+if x:
+  print("Yes, there is at least one match!")
+else:
+  print("No match")
+
+# $ python3 special-sequences-10.py 
+# ['T', 'h', 'e', 'r', 'a', 'i', 'n', 'i', 'n', 'S', 'p', 'a', 'i', 'n']
+# Yes, there is at least one match!
