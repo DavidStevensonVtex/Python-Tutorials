2d3c60feb4bc0bf179ef127b46f5495a5f0c52a3
David Stevenson <drstevenson1958@yahoo.com>
2025-01-27 09:21:18 -0500

    W3Schools: 032-Python-RegExp: special sequences \A:	Returns a match if the specified characters are at the beginning of the string: Example	"\AThe"

---
+++ W3Schools/Python-Tutorial/032-Python-RegEx/special-sequences/special-sequences-01.py
@@ -0,0 +1,18 @@
+import re
+
+txt = "The rain in Spain"
+
+#Check if the string starts with "The":
+
+x = re.findall("\AThe", txt)
+
+print(x)
+
+if x:
+  print("Yes, there is a match!")
+else:
+  print("No match")
+
+# $ python3 special-sequences-01.py 
+# ['The']
+# Yes, there is a match!
