87a2ca2f4b118bc63fef09f96ecaa0b2c22b55e5
David Stevenson <drstevenson1958@yahoo.com>
2025-01-27 09:43:32 -0500

    W3Schools: 032-Python-RegExp: special sequences \S:	Returns a match where the string DOES NOT contain a white space character: Example "\S"

---
+++ W3Schools/Python-Tutorial/032-Python-RegEx/special-sequences/special-sequences-09.py
@@ -0,0 +1,18 @@
+import re
+
+txt = "The rain in Spain"
+
+#Return a match at every NON white-space character:
+
+x = re.findall("\S", txt)
+
+print(x)
+
+if x:
+  print("Yes, there is at least one match!")
+else:
+  print("No match")
+
+# $ python3 special-sequences-09.py 
+# ['T', 'h', 'e', 'r', 'a', 'i', 'n', 'i', 'n', 'S', 'p', 'a', 'i', 'n']
+# Yes, there is at least one match!
