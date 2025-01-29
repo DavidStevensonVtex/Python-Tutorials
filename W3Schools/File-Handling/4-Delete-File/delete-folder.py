cef71e4c616230a418de037bbdc755636c1ef3aa
David Stevenson <drstevenson1958@yahoo.com>
2025-01-29 13:40:45 -0500

    W3Schools: Python File Handling: Delete Folder

---
+++ W3Schools/File-Handling/4-Delete-File/delete-folder.py
@@ -0,0 +1,11 @@
+# Delete Folder
+# To delete an entire folder, use the os.rmdir() method:
+
+# Example
+# Remove the folder "myfolder":
+
+# To create the folder to be removed:
+# $ mkdir myfolder
+
+import os
+os.rmdir("myfolder")
