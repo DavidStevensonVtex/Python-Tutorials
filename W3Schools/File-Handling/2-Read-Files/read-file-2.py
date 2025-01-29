8ff42fa0a7d450432a7b32172cdabce24da34c85
David Stevenson <drstevenson1958@yahoo.com>
2025-01-29 12:57:49 -0500

    W3Schools: File Handling: Read text file in different directory

---
+++ W3Schools/File-Handling/2-Read-Files/read-file-2.py
@@ -0,0 +1,11 @@
+# If the file is located in a different location, 
+# you will have to specify the file path, like this:
+
+# Example
+# Open a file on a different location:
+
+f = open("../1-File-Open/demofile.txt", "r")
+print(f.read())
+
+# $ python3 read-file-2.py 
+# The rain in Spain
