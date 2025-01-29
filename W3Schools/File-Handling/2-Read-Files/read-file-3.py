b87d70dd7473ab0e87fee0c0a190036165616c3d
David Stevenson <drstevenson1958@yahoo.com>
2025-01-29 12:58:59 -0500

    W3Schools: File Handling: Read Only Parts of the File

---
+++ W3Schools/File-Handling/2-Read-Files/read-file-3.py
@@ -0,0 +1,12 @@
+# Read Only Parts of the File
+# By default the read() method returns the whole text, but you can also specify 
+# how many characters you want to return:
+
+# Example
+# Return the 5 first characters of the file:
+
+f = open("demofile.txt", "r")
+print(f.read(5))
+
+# $ python3 read-file-3.py 
+# Hello
