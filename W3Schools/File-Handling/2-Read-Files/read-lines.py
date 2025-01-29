5a0867252e9dbd432d1e93b198de4bbb31e5506c
David Stevenson <drstevenson1958@yahoo.com>
2025-01-29 13:00:02 -0500

    W3Schools: File Handling: Read Lines

---
+++ W3Schools/File-Handling/2-Read-Files/read-lines.py
@@ -0,0 +1,12 @@
+# Read Lines
+# You can return one line by using the readline() method:
+
+# Example
+# Read one line of the file:
+
+f = open("demofile.txt", "r")
+print(f.readline())
+
+# $ python3 read-lines.py 
+# Hello! Welcome to demofile.txt
+#
