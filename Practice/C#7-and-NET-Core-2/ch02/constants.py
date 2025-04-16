d9f91df8fa2bd69a7af1a0524f9941026e69a448
David Stevenson <drstevenson1958@yahoo.com>
2025-04-16 13:55:37 -0400

    Pythong Practice using C# 7 and .NET Core 2.0: Chapter 2:  Working with Constants

---
+++ Practice/C#7-and-NET-Core-2/ch02/constants.py
@@ -0,0 +1,8 @@
+A = 100
+B = "The rain in spain falls mainly in the plain."
+EULERS_CONSTANT = 2.71828
+
+print(A, B, EULERS_CONSTANT)
+
+# $ python constants.py 
+# 100 The rain in spain falls mainly in the plain. 2.71828
