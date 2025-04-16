4292b71eff6120f2dcf265fd3c9de21a9124fd3e
David Stevenson <drstevenson1958@yahoo.com>
2025-04-16 14:04:27 -0400

    Python Practice using C# and .NET Core 2.0: Chapter 2: Digit Separators and formatting output using comma separator , binary, octal and hex format

---
+++ Practice/C#7-and-NET-Core-2/ch02/digit-separators.py
@@ -0,0 +1,9 @@
+MILLION = 1_000_000
+BINARY_NUMBER = 0b1010_0011
+OCTAL_NUMBER = 0o755_644
+HEX_NUMBER = 0xFF_FF_FF
+
+print(f"million: {MILLION:,} binary: {bin(BINARY_NUMBER)} octal: {oct(OCTAL_NUMBER)} hex: {hex(HEX_NUMBER)}")
+
+# $ python digit-separators.py 
+# million: 1,000,000 binary: 0b10100011 octal: 0o755644 hex: 0xffffff
