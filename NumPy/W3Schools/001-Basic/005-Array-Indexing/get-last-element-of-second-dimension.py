9b6fb5c5d254f7610a5c371a0937a7c965b7230e
David Stevenson <drstevenson1958@yahoo.com>
2025-04-21 17:53:17 -0400

    NumPy: W3Schools: NumPy Array Indexing: Access 3-D Arrays, Negative Indexing

---
+++ NumPy/W3Schools/001-Basic/005-Array-Indexing/get-last-element-of-second-dimension.py
@@ -0,0 +1,10 @@
+# Print the last element from the 2nd dim:
+
+import numpy as np
+
+arr = np.array([[1,2,3,4,5], [6,7,8,9,10]])
+
+print('Last element from 2nd dim: ', arr[1, -1])
+
+# $ python get-last-element-of-second-dimension.py 
+# Last element from 2nd dim:  10
