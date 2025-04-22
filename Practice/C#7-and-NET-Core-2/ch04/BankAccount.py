018fd5208d7b7f4fd075b648795f68ea6ea197bf
David Stevenson <drstevenson1958@yahoo.com>
2025-04-22 15:40:14 -0400

    Python Practice: Chapter 4: Interface Inheritance: CurrentAccount  (inherits from AbstractTransferAccount, which inherits from AbstractBankAccount) implements transfer_to class method

--- Practice/C#7-and-NET-Core-2/ch04/BankAccount.py
+++ Practice/C#7-and-NET-Core-2/ch04/BankAccount.py
@@ -2,12 +2,15 @@ from abc import ABC, abstractmethod
 
 
 class AbstractBankAccount(ABC):
+    @abstractmethod
     def pay_in(amount):
         pass
 
+    @abstractmethod
     def withdraw(amount):
         pass
 
+    @abstractmethod
     def balance():
         pass
 
@@ -33,21 +36,63 @@ class SaverAccount(AbstractBankAccount):
         return f"Venus Bank Saver: Balance = {self.__balance}"
 
 
-aba = AbstractBankAccount()
+class AbstractTransferBankAccount(AbstractBankAccount):
+    @abstractmethod
+    def transfer_to(destination: AbstractBankAccount, amount: float):
+        pass
+
+
+# TypeError: Can't instantiate abstract class CurrentAccount with abstract methods balance, pay_in, transfer_to, withdraw
+class CurrentAccount(AbstractTransferBankAccount):
+    def __init__(self, balance):
+        self.__balance = balance
+
+    def pay_in(self, amount):
+        self.__balance += amount
+
+    def withdraw(self, amount):
+        if self.__balance > amount:
+            self.__balance -= amount
+            return True
+        print("Withdrawal attempt failed.")
+        return False
+
+    def balance(self):
+        return self.__balance
+
+    def transfer_to(self, destination, amount):
+        result = self.withdraw(amount)
+        if result:
+            destination.pay_in(amount)
+        return result
+
+    def __str__(self):
+        return f"Jupiter Bank Saver: Balance = {self.__balance}"
+
+
+# TypeError: Can't instantiate abstract class AbstractBankAccount with abstract methods balance, pay_in, withdraw
+# aba = AbstractBankAccount()
+
+current_account = CurrentAccount(2000)
+print("After opening Jupiter account with $2000:", current_account)
 
 sa = SaverAccount(500)
-print("After opening account with $500: ", sa)
+print("After opening Venus account with $500:", sa)
 
-sa.pay_in(250)
-print("After $250 deposit: ", sa)
+current_account.transfer_to(sa, 500)
+print("After transfering $500 from Jupiter account to Venus account:", sa)
+print("Jupiter account balance:", current_account)
 
-success = sa.withdraw(125)
-print(f"Succesful withdrawal: {success}")
-print("After $125 withdrawal: ", sa)
+# sa.pay_in(250)
+# print("After $250 deposit: ", sa)
 
-success = sa.withdraw(9999)
-print(f"Succesfull withdrawal: {success}")
-print("After $9999 withdrawal: ", sa)
+# success = sa.withdraw(125)
+# print(f"Succesful withdrawal: {success}")
+# print("After $125 withdrawal: ", sa)
+
+# success = sa.withdraw(9999)
+# print(f"Succesfull withdrawal: {success}")
+# print("After $9999 withdrawal: ", sa)
 
 # $ python BankAccount.py
 # After opening account with $500:  Venus Bank Saver: Balance = 500
@@ -57,3 +102,10 @@ print("After $9999 withdrawal: ", sa)
 # Withdrawal attempt failed.
 # Succesfull withdrawal: False
 # After $9999 withdrawal:  Venus Bank Saver: Balance = 625
+
+
+# $ python BankAccount.py 
+# After opening Jupiter account with $2000: Jupiter Bank Saver: Balance = 2000
+# After opening Venus account with $500: Venus Bank Saver: Balance = 500
+# After transfering $500 from Jupiter account to Venus account: Venus Bank Saver: Balance = 1000
+# Jupiter account balance: Jupiter Bank Saver: Balance = 1500
