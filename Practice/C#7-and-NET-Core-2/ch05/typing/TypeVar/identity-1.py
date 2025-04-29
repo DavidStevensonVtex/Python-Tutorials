dfdf00a1e6652cb83b1e88582b8ab77f258af3f4
David Stevenson <drstevenson1958@yahoo.com>
2025-04-29 14:39:31 -0400

    Python Practice: Chapter 5: Generics: Typing: TypeVars: identity example - runtime failure though type checking passes

--- Practice/C#7-and-NET-Core-2/ch05/typing/TypeVar/identity-1.py
+++ Practice/C#7-and-NET-Core-2/ch05/typing/TypeVar/identity-1.py
@@ -1,5 +1,16 @@
+from typing import Any
+
+
 def identity(arg: Any) -> Any:
     return arg
 
 
-print(identity(identity(42)))
+number = identity(42)
+print(number + "!")
+
+# $ python identity-1.py
+# Traceback (most recent call last):
+#   File "D:\drs\Python\GitHub\Python-Tutorials\Practice\C#7-and-NET-Core-2\ch05\typing\TypeVar\identity-1.py", line 9, in <module>
+#     print(number + "!")
+#           ~~~~~~~^~~~~
+# TypeError: unsupported operand type(s) for +: 'int' and 'str'
